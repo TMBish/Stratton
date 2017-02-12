ed2a4ec41eece5769750e581561fbec6c6d86f51
Tom Bishop <tomwbish@gmail.com>
2017-02-12 12:46:47 +1100

    Observer to construct data set working
    Renders a data table on the fron page for inspection

--- App/server.R
+++ App/server.R
@@ -3,39 +3,41 @@ library(shiny)
 
 # Server logic
 shinyServer(function(input, output, session) {
-   
   
-  # Event to run when user selects new person
+  
+  revals = reactiveValues(
+    data_set = NULL, # Init data set as null
+    do_plot = FALSE # Turn off displays when App is opened
+  )
+  
+  
+  # Update data set on user search
   observeEvent(input$search, {
     
     # Alert the client
     session$sendCustomMessage(type = 'testmessage', message = list(input$search_input))
     
-    # Currently selected actor
-    sel_search = input$search_input
-    
     # Get rotten tomatoes data for this person
-    sel_films = get_tomatoes(sel_search)
+    sel_films = get_tomatoes(input$search_input)
     
     # Get box office data for this person
     sel_films_comp = get_box_office(sel_films)
-    
+
     # Save as the master data-set
-    data_set = sel_films_comp
-    
-    
+    revals$data_set = sel_films_comp
     
   })
   
-  output$data_set = renderDataTable({
-    
-    return(data_set)
-    
+  output$data_set = 
+    renderDataTable(revals$data_set,
+                    options = list(
+                      pageLength = 5,
+                      initComplete = I("function(settings, json) {alert('Done.');}")
+                    ))
+  
+  
+  
+  
+  
+  
   })
-  
-  
-  
-  
-  
-  
-})
