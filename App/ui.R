c0dca08a1e2c5a8652c7db6fb6a1260c40722204
Tom Bishop <tomwbish@gmail.com>
2017-02-19 13:31:54 +1100

    UI: added header

--- App/ui.R
+++ App/ui.R
@@ -13,67 +13,77 @@ shinyUI(
       tags$head(
         includeScript(file.path('www', 'message-handler.js')),
         includeCSS(file.path('www', 'style.css'))
-      )
-    ),
-    
-    
-    # Arvo google font
-    tags$style('<link href="https://fonts.googleapis.com/css?family=Arvo" rel="stylesheet">')
-    
-    
-  ),
-  
-  # Application Body --------------------------------------------------------
-  
-  # Application title
-  titlePanel("Stratton: an interactive tool for films"),
-  
-  
-  # Sidebar -----------------------------------------------------------------
-  # Sidebar with a slider input for number of bins 
-  sidebarLayout(
-    sidebarPanel(id="sidebar",
-                 
-                 # Actor / Director Input Box
-                 h3("Enter the name of an Actor or Director:"),
-                 
-                 textInput.typeahead(
-                   id="search_input",
-                   placeholder="e.g. Joseph Gordon-Levitt",
-                   local= typeahead_data,
-                   valueKey = "Name",
-                   tokens=seq(1,nrow(typeahead_data)),
-                   template = HTML("<p class='repo-language'>{{Role}}</p> <p class='repo-name'>{{Name}}</p>")
-                 ), 
-                 
-                 
-                 
-                 bsButton("search", 
-                          "Cluster",
-                          icon = icon("refresh"),
-                          style = "info"
-                 )
-    ),
-    
-    
-    
-    
-    # Body --------------------------------------------------------------------
-    mainPanel(
+      ),
+      # Arvo google font
+      tags$style('<link href="https://fonts.googleapis.com/css?family=Arvo" rel="stylesheet">')
       
-      tags$div(id = "body_div",
-               
-               tabsetPanel(
-                 tabPanel("Chart", plotOutput('chart')), 
-                 tabPanel("Timeline"), 
-                 tabPanel("Raw Data", dataTableOutput("data_set"))
-               )
-               
-               
+    ),
+    
+    # Application Body --------------------------------------------------------
+    
+    # App Header
+    div(id = "headerSection",
+        h1("Stratton: an interactive tool for films"),
+        
+        # author info
+        span(
+          style = "font-size: 1.2em",
+          span("Created by "),
+          a("Tom Bishop", href = "http://deanattali.com"),
+          HTML("&bull;"),
+          span("Code"),
+          a("on GitHub", href = "https://github.com/TMBish/Stratton"),
+          HTML("&bull;")
+        )
+    ),
+    
+    
+    # Sidebar -----------------------------------------------------------------
+    # Sidebar with a slider input for number of bins 
+    sidebarLayout(
+      sidebarPanel(id="sidebar",
+                   
+                   # Actor / Director Input Box
+                   h3("Enter the name of an Actor or Director:"),
+                   
+                   textInput.typeahead(
+                     id="search_input",
+                     placeholder="e.g. Joseph Gordon-Levitt",
+                     local= typeahead_data,
+                     valueKey = "Name",
+                     tokens=seq(1,nrow(typeahead_data)),
+                     template = HTML("<p class='repo-language'>{{Role}}</p> <p class='repo-name'>{{Name}}</p>")
+                   ), 
+                   
+                   
+                   
+                   bsButton("search", 
+                            "Cluster",
+                            icon = icon("refresh"),
+                            style = "info"
+                   )
+      ),
+      
+      
+      
+      
+      # Body --------------------------------------------------------------------
+      
+      mainPanel(
+        wellPanel(
+          tags$div(id = "body_div",
+                   
+                   tabsetPanel(
+                     tabPanel("Chart", plotOutput('chart')), 
+                     tabPanel("Timeline"), 
+                     tabPanel("Raw Data", dataTableOutput("data_set"))
+                   )
+                   
+          )          
+        )
       )
     )
-  )
-))
+  ))
 
 
 
