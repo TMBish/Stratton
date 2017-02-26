faf80952ee4aad08ae3a527c2aad62434b2a3528
Tom Bishop <tomwbish@gmail.com>
2017-02-26 13:55:48 +1100

    Encircling of clusters functionality added

--- App/global.R
+++ App/global.R
@@ -238,17 +238,20 @@ chart_cluster = function(df, axes = c("rating", "intl_revenue")) {
   
   require(ggplot2)
   require(hrbrthemes)
+  require(ggalt)
   require(gcookbook)
+  require(RColorBrewer)
   
   x_lab = switch(axes[2], "rating" = "Rotten Tomatoes Score"
-                        , "intl_revenue" = "Box Office Revenue")
+                 , "intl_revenue" = "Box Office Revenue")
   y_lab = switch(axes[1], "rating" = "Rotten Tomatoes Score"
-                        , "intl_revenue" = "Box Office Revenue")
+                 , "intl_revenue" = "Box Office Revenue")
   
-  ggplot(data = df, aes_string(y = axes[1], x = axes[2], color = "cluster")) +
+  gg = ggplot(data = df, aes_string(y = axes[1], x = axes[2], color = "cluster")) +
     geom_point(size = 3) +
-    scale_y_continuous(limits = c(0,100)) +
-    scale_x_comma() +
+    scale_y_continuous(limits = c(0, 1.1 * max(df[,axes[1]]))) +
+    scale_x_comma(limits = c(0, 1.1 * max(df[,axes[2]]))) +
+    scale_color_brewer(type = 'qual', palette = 'Accent') +
     labs(x=x_lab, y=y_lab,
          title="Test plot",
          subtitle="A plot that is only useful for demonstration purposes",
@@ -256,5 +259,20 @@ chart_cluster = function(df, axes = c("rating", "intl_revenue")) {
     theme_ipsum_rc(grid="XY") +
     theme(axis.line.x = element_line(color="black"),
           axis.line.y = element_line(color="black"))
+  
+  # Encircle clusters
+  ind = 1
+  for (clust in unique(df$cluster)) {
     
+    gg = gg + geom_encircle(
+      data = filter(df, cluster == clust),
+      color = brewer.pal(length(unique(df$cluster)), name = "Accent")[ind],
+      s_shape=0.9, expand=0.05
+    )
+    
+    ind = ind + 1
+  }
+  
+  return(gg)
+  
 }
